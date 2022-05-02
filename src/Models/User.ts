c01d9d05cf896aea71ca37a1deebeebcaef44f11
Roman Opalacz <roman9891@gmail.com>
2022-05-02 17:00:54 -0400

    added axios and fetch to user

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios'
+
 interface UserProps {
+  id?: number
   name?: string
   age?: number
 }
@@ -30,5 +33,13 @@ export class User {
     if (!handlers || handlers.length === 0) return
 
     handlers.forEach((callback) => callback())
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
   }
 }
